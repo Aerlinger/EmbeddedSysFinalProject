77da3567876b4c60424339e3cd3d6ca629fed1a7
Anthony Erlinger <aerlinger@gmail.com>
2013-05-12 21:51:03 -0400

    Fix indentations in CPU.vhd (pending verification)

--- vhdl/alu.vhd
+++ vhdl/alu.vhd
@@ -26,11 +26,11 @@ use ieee.std_logic_arith.all;
 entity ALU is
 
   port(
-    AI:  in std_logic_vector(7 downto 0);
-    BI:  in std_logic_vector(7 downto 0);
-    ADD:  out std_logic_vector(7 downto 0);
-    P: in std_logic_vector(7 downto 0);
-    opcode: in std_logic_vector(7 downto 0);
+    AI:   in unsigned(7 downto 0);
+    BI:   in unsigned(7 downto 0);
+    ADD:  out unsigned(7 downto 0);
+    P:    in unsigned(7 downto 0);
+    opcode: in unsigned(7 downto 0);
 
     ANDS: in std_logic;
     SUMS: in std_logic;
@@ -54,22 +54,22 @@ architecture rtl of ALU is
   signal temp : unsigned(8 downto 0);
 begin
 
-    process(AI, BI, ANDS, SUMS, ORS, EORS, SRS, I_ADDC)
-    begin
+  process(AI, BI, ANDS, SUMS, ORS, EORS, SRS, I_ADDC)
+  begin
 
-      if (SUMS='1') then
-        temp <= x"00"; --('0'&AI) + ('0'&BI) + ("0"&I_ADDC); -- stubbed for now...
-        ADD <= std_logic_vector(temp(7 downto 0));
-        ACR <= std_logic(temp(8) or Mask_shortcut);
-      elsif (ANDS='1') then
-        ADD <= AI and BI;
-      elsif (ORS='1') then 
-        ADD <= AI or BI;
-      elsif (EORS='1') then
-        ADD <= AI xor BI;
+    if (SUMS='1') then
+      temp <= x"00"; --('0'&AI) + ('0'&BI) + ("0"&I_ADDC); -- stubbed for now...
+      ADD <= std_logic_vector(temp(7 downto 0));
+      ACR <= std_logic(temp(8) or Mask_shortcut);
+    elsif (ANDS='1') then
+      ADD <= AI and BI;
+    elsif (ORS='1') then 
+      ADD <= AI or BI;
+    elsif (EORS='1') then
+      ADD <= AI xor BI;
 
-      -- TOOD: Verify this is how shifting works in the 6502
-      elsif (SRS='1') then
+    -- TOOD: Verify this is how shifting works in the 6502
+    elsif (SRS='1') then
 --        ADD(6 downto 0) <= BI(7 downto 1);
 --        ACR <= BI(0);
 --        if opcode(7 downto 5)="010" then
@@ -77,11 +77,11 @@ begin
 --        elsif opcode(7 downto 5)="011" then
 --          ADD(7) <= P(0);
 --        end if;
-      else
-        ADD <= "XXXXXXXX";
-      end if;
+    else
+      ADD <= "XXXXXXXX";
+    end if;
 
-    end process;
+  end process;
   -- use case statement to achieve different operations of ALU
 
 end rtl;
